acd46535160e070e1930d09b78ecbea12126722e
skyman <lexbanasihan21@gmail.com>
2024-09-21 18:07:08 +0800

    fixed

---
+++ lib/screens/dashboard/studentDashboard/votingsystem_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VotingsystemHome extends StatelessWidget {
+  const VotingsystemHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('HOME SCREEN'),
+      ),
+    );
+  }
+}
