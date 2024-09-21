f1d642792ac6ad6a664acf2e16edde2be9734032
skyman <lexbanasihan21@gmail.com>
2024-09-21 21:53:30 +0800

    added border

--- lib/screens/dashboard/dashboardContents/candidate_widget.dart
+++ lib/screens/dashboard/dashboardContents/candidate_widget.dart
@@ -13,34 +13,42 @@ class CandidateWidget extends StatefulWidget {
 class _CandidateWidgetState extends State<CandidateWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Container(
-              height: 200,
-              width: 150,
-              child: Image.network(
-                widget.candidate.imageUrl,
-                fit: BoxFit.cover, // Ensures the image covers the space
-                errorBuilder: (context, error, stackTrace) {
-                  return const Center(child: Text('Image failed to load'));
-                },
+    return Expanded(
+      child: Container(
+        decoration:
+            BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
+        margin: EdgeInsets.all(8),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 200,
+                width: 150,
+                child: Image.network(
+                  widget.candidate.imageUrl,
+                  fit: BoxFit.cover, // Ensures the image covers the space
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Center(child: Text('Image failed to load'));
+                  },
+                ),
               ),
-            ),
-            const SizedBox(height: 8), // Spacing
-            Text(
-              widget.candidate.name,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4), // Spacing
-            Text(
-              widget.candidate.motto,
-              style: const TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              const SizedBox(height: 8), // Spacing
+              Text(
+                widget.candidate.name,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 4), // Spacing
+              Text(
+                widget.candidate.motto,
+                style:
+                    const TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/dashboard/studentDashboard/votingsystem_home.dart
+++ lib/screens/dashboard/studentDashboard/votingsystem_home.dart
@@ -2,65 +2,139 @@ import 'package:flutter/material.dart';
 import 'package:voting_system/core/models/candidate_model.dart';
 import 'package:voting_system/screens/dashboard/dashboardContents/candidate_widget.dart';
 
-class VotingsystemHome extends StatelessWidget {
+class VotingsystemHome extends StatefulWidget {
   const VotingsystemHome({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Sample list of candidates
-    final List<Candidate> candidates = [
-      Candidate(
-        imageUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg/1200px-Andrzej_Person_Kancelaria_Senatu.jpg',
-        name: 'John Doe',
-        motto: 'For a better future',
-      ),
-      Candidate(
-        imageUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
-        name: 'Jane Smith',
-        motto: 'Together we can!',
-      ),
-      Candidate(
-        imageUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
-        name: 'Jane Smith',
-        motto: 'Together we can!',
-      ),
-      Candidate(
-        imageUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
-        name: 'Jane Smith',
-        motto: 'Together we can!',
-      ),
-      Candidate(
-        imageUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
-        name: 'Jane Smith',
-        motto: 'Together we can!',
-      ),
-      Candidate(
-        imageUrl:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr74Pjdj__bQPnZK-BFujbwgnP1t5PIqkig&s',
-        name: 'Alice Johnson',
-        motto: 'Your voice matters',
-      ),
-      // Add more candidates as needed
-    ];
+  _VotingsystemHomeState createState() => _VotingsystemHomeState();
+}
 
+class _VotingsystemHomeState extends State<VotingsystemHome> {
+  List<Candidate> candidates = [
+    Candidate(
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg/1200px-Andrzej_Person_Kancelaria_Senatu.jpg',
+      name: 'John Doe',
+      motto:
+          'Empowering Change for a Brighter Tomorrow: Together, we can build a future where every voice is heard and every dream is realizede',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
+      name: 'Jane Smith',
+      motto:
+          'Together We Rise, Together We Thrive: By uniting our strengths and embracing our differences, we can create a community where everyone has the opportunity to succeed',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr74Pjdj__bQPnZK-BFujbwgnP1t5PIqkig&s',
+      name: 'Alice Johnson',
+      motto:
+          'Your Voice, Our Future: Join us in shaping a tomorrow that reflects our shared values and aspirations, where everyone plays a vital role in the journey ahead.',
+    ),
+    Candidate(
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Andrzej_Person_Kancelaria_Senatu.jpg/1200px-Andrzej_Person_Kancelaria_Senatu.jpg',
+      name: 'John Doe',
+      motto:
+          'Unity in Diversity, Strength in Community: Embracing our unique perspectives, we can foster an environment that promotes collaboration, understanding, and progress for all',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
+      name: 'Jane Smith',
+      motto:
+          'Innovating for Progress and Prosperity: Together, we will develop innovative solutions that empower individuals and uplift communities, driving positive change for generations to come',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr74Pjdj__bQPnZK-BFujbwgnP1t5PIqkig&s',
+      name: 'Alice Johnson',
+      motto:
+          'Innovating for Progress and Prosperity: Together, we will develop innovative solutions that empower individuals and uplift communities, driving positive change for generations to come',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4xjShh4ynJbrgYrW_aB4lhKSxeMzQ3cO_A&s',
+      name: 'Jane Smith',
+      motto:
+          'Innovating for Progress and Prosperity: Together, we will develop innovative solutions that empower individuals and uplift communities, driving positive change for generations to come',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr74Pjdj__bQPnZK-BFujbwgnP1t5PIqkig&s',
+      name: 'Alice Johnson',
+      motto:
+          'Innovating for Progress and Prosperity: Together, we will develop innovative solutions that empower individuals and uplift communities, driving positive change for generations to come',
+    ),
+    Candidate(
+      imageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHr74Pjdj__bQPnZK-BFujbwgnP1t5PIqkig&s',
+      name: 'Alice Johnson',
+      motto:
+          'Your voice matters, Your voice matters, Your voice matters, Your voice matters, Your voice matters, Your voice matters, Your voice matters',
+    ),
+    // Add more candidates as needed
+  ];
+
+  List<Candidate> filteredCandidates = [];
+  String searchQuery = '';
+
+  @override
+  void initState() {
+    super.initState();
+    filteredCandidates = candidates; // Initialize with all candidates
+  }
+
+  void _filterCandidates(String query) {
+    setState(() {
+      searchQuery = query;
+      filteredCandidates = candidates
+          .where((candidate) =>
+              candidate.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 350,
-            mainAxisExtent: 300 // Maximum width of each item
+      child: Column(
+        children: [
+          // Search Bar
+          Padding(
+            padding: const EdgeInsets.only(bottom: 8.0, left: 8, right: 8),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search candidates...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.green),
+                ),
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: _filterCandidates,
             ),
-        itemCount: candidates.length,
-        itemBuilder: (context, index) {
-          return CandidateWidget(
-            candidate: candidates[index], // Use CandidateWidget
-          );
-        },
+          ),
+          // Candidate Grid
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 350, mainAxisExtent: 450),
+              itemCount: filteredCandidates.length,
+              itemBuilder: (context, index) {
+                return CandidateWidget(
+                  candidate: filteredCandidates[index], // Use CandidateWidget
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
